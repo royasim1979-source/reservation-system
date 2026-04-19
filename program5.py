9d767e0c2b6d4fab708d8cf938971e39f69da025
royasim1979-source <royasim1979@gmail.com>
2026-04-19 14:52:23 +0100

    Delete program5.py

--- program5.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-arr = np.array([[-2,-2,-4],[0,1,0],[-11,17,3]])
-print(np.linalg.inv(arr))
-arr1 = np.array([4,8,4])
-print(np.unique(arr1))
-arr2 = np.array([[1],[9],[5]])
-print(arr+arr1)
-print(arr1+arr2)
